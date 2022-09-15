13946737a7ac74f170d36b388613737ff01bc8ff
joseljg <joseljg@gmail.com>
2022-09-15 19:16:15 +0200

    Initial commit

---
+++ app/src/main/java/es/joseljg/holamundo_22_23/MainActivity.java
@@ -0,0 +1,14 @@
+package es.joseljg.holamundo_22_23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
